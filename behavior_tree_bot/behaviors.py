1b80326a53b69bf32e5a8ed211596acaa436391d
pvarade7 <116333490+pvarade7@users.noreply.github.com>
2024-07-14 23:51:55 +0000

    error still occurring

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -9,7 +9,6 @@ def attack_strongest_enemy(state):
 
     # (2) Find my strongest planet.
     strongest_planet = max(state.my_planets(), key=lambda p: p.num_ships, default=None)
-
     if not strongest_planet:
         return False
 
@@ -21,8 +20,7 @@ def attack_strongest_enemy(state):
     largest_beatable_enemy = max(beatable_enemy_planets, key=lambda p: p.num_ships)
 
     # (4) Send half the ships from my strongest planet to the largest beatable enemy planet.
-    return issue_order(state, strongest_planet.ID, largest_beatable_enemy.ID, strongest_planet.num_ships // 2)
-
+    return issue_order(state, strongest_planet.ID, largest_beatable_enemy.ID, strongest_planet.num_ships / 2)
 
 
 def attack_weakest_neutral(state):
@@ -32,17 +30,20 @@ def attack_weakest_neutral(state):
 
     # (2) Find my weakest planet.
     weakest_planet = min(state.my_planets(), key=lambda p: p.num_ships, default=None)
-
-    # (3) Find the weakest neutral planet.
-    weakest_neutral_planet = min(state.neutral_planets(), key=lambda p: p.num_ships, default=None)
-
-    if not weakest_planet or not weakest_neutral_planet:
-        # No legal source or destination
+    if not weakest_planet:
         return False
-    else:
-        # (4) Send half the ships from my weakest planet to the weakest neutral planet.
-        return issue_order(state, weakest_planet.ID, weakest_neutral_planet.ID, weakest_planet.num_ships // 2)
 
+    # (3) Sort neutral planets by the number of ships in ascending order.
+    neutral_planets_sorted = sorted(state.neutral_planets(), key=lambda p: p.num_ships)
+    if not neutral_planets_sorted:
+        return False
+
+    # (4) Find the weakest beatable neutral planet and attack.
+    for neutral_planet in neutral_planets_sorted:
+        if weakest_planet.num_ships > neutral_planet.num_ships:
+            return issue_order(state, weakest_planet.ID, neutral_planet.ID, weakest_planet.num_ships / 2)
+
+    return False
 
 def attack_weakest_enemy_planet(state):
     # (1) If we currently have a fleet in flight, abort plan.
